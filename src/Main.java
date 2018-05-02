1d77690ad7b4743853c8e1a6dbd1c691739fd940
MorganRush <matvey159@gmail.com>
2018-05-03 02:13:39 +0300

    release

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import logical.Graph;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Graph graph = new Graph();
-        graph.test2();
-        int result = graph.findMaxFlow(0, 3);
-        System.out.println(result);
+        int result = graph.myVariantBandwidthZero(0, 5);
+        System.out.println("Максимальный поток: " + result);
+        System.out.println();
+        result = graph.myVariantBandwidthNotZero(0, 5);
+        System.out.println("Максимальный поток: " + result);
     }
 }
--- src/logical/Graph.java
+++ src/logical/Graph.java
@@ -1,16 +1,13 @@
 package logical;
 
-import com.sun.javafx.geom.Edge;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Graph {
 
     private int countNode;
 
     private static final int infinity = 100;
-    //пропускная способность
+
     private int[][] bandwidth;
 
     private int[][] flow;
@@ -41,9 +38,14 @@ public class Graph {
 
     private boolean findPath(int source, int target){
 
+        if (target > countNode){
+            target = countNode;
+        }
+
         ArrayList<Edge> edgesFromChain = new ArrayList<Edge>();
         int minFlowFromChain = infinity;
         int thisIndex = source;
+        int indexCheck = -1;
 
         while (thisIndex != target){
             ArrayList<Edge> edgesIncident = new ArrayList<Edge>();
@@ -57,14 +59,15 @@ public class Graph {
                     }
                 }
                 if (!isIEqualsIndexPrevious && i != source && !isSeen[i]){
-                    if (bandwidth[thisIndex][i] != 0){
-                        edgesIncident.add(new Edge(i, true));
-                        if (bandwidth[thisIndex][i] > maxBandwidth){
-                            indexMax = edgesIncident.size() - 1;
-                            maxBandwidth = bandwidth[thisIndex][i];
+                    if (bandwidth[thisIndex][i] >= flow[i][thisIndex]){
+                        if (bandwidth[thisIndex][i] != 0){
+                            edgesIncident.add(new Edge(i, true));
+                            if (bandwidth[thisIndex][i] > maxBandwidth){
+                                indexMax = edgesIncident.size() - 1;
+                                maxBandwidth = bandwidth[thisIndex][i];
+                            }
                         }
-                    }
-                    if (flow[i][thisIndex] > 0){
+                    } else if (flow[i][thisIndex] > 0){
                         edgesIncident.add(new Edge(i, false));
                         if (flow[i][thisIndex] > maxBandwidth){
                             indexMax = edgesIncident.size() - 1;
@@ -73,13 +76,20 @@ public class Graph {
                     }
                 }
             }
+            if (edgesIncident.size() > 1){
+                indexCheck = edgesIncident.get(indexMax).indexNode;
+            }
             if (indexMax == -1){
-                int indexIsSeen = 0;
-                if (!edgesFromChain.isEmpty()){
-                    indexIsSeen = edgesFromChain.get(0).getIndexNode();
-                    edgesFromChain.clear();
+                if (indexCheck == -1){
+                    int indexIsSeen = 0;
+                    if (!edgesFromChain.isEmpty()){
+                        indexIsSeen = edgesFromChain.get(0).getIndexNode();
+                    }
+                    isSeen[indexIsSeen] = true;
+                } else {
+                    isSeen[indexCheck] = true;
                 }
-                isSeen[indexIsSeen] = true;
+                edgesFromChain.clear();
                 break;
             }
             Edge edgeToChain = edgesIncident.get(indexMax);
@@ -90,23 +100,35 @@ public class Graph {
             thisIndex = edgeToChain.indexNode;
         }
         int start = source;
-        for (Edge edge : edgesFromChain) {
-            int next = edge.getIndexNode();
-            if (edge.isToFlow()){
-                flow[start][next] += minFlowFromChain;
-                bandwidth[start][next] -= minFlowFromChain;
+        if (!edgesFromChain.isEmpty()){
+            System.out.println("Увеличивающая цепь:");
+            for (Edge edge : edgesFromChain) {
+                int next = edge.getIndexNode();
+                System.out.println("Ребро: " + start + " " + next);
+                if (edge.isToFlow()){
+                    System.out.println("Текущий поток: " + flow[start][next]);
+                    System.out.println("Текущая пропускная способность: " + bandwidth[start][next]);
+                    flow[start][next] += minFlowFromChain;
+                    bandwidth[start][next] -= minFlowFromChain;
+                    System.out.println("Новый поток: " + flow[start][next]);
+                    System.out.println("Новая пропускная способность: " + bandwidth[start][next]);
+                }
+                else {
+                    System.out.println("Текущий поток: " + flow[next][start]);
+                    System.out.println("Текущая пропускная способность: " + bandwidth[next][start]);
+                    flow[next][start] -= minFlowFromChain;
+                    bandwidth[next][start] += minFlowFromChain;
+                    System.out.println("Новый поток: " + flow[next][start]);
+                    System.out.println("Новая пропускная способность: " + bandwidth[next][start]);
+                }
                 start = next;
             }
-            else {
-                flow[next][start] -= minFlowFromChain;
-                bandwidth[next][start] += minFlowFromChain;
-                start = next;
-            }
+            System.out.println();
         }
         return !isSeen[0];
     }
 
-    public int findMaxFlow(int source, int target){
+    private int findMaxFlow(int source, int target){
         for (int i = 0; i < isSeen.length; i++){
             isSeen[i] = false;
         }
@@ -121,7 +143,7 @@ public class Graph {
         return maxFlow;
     }
 
-    public void test1(){
+    public int test1(int source, int target){
         countNode = 6;
         bandwidth = new int[countNode][countNode];
         flow = new int [countNode][countNode];
@@ -134,9 +156,11 @@ public class Graph {
         bandwidth[3][5] = 10;
         bandwidth[4][3] = 4;
         bandwidth[4][5] = 7;
+
+        return findMaxFlow(source, target);
     }
 
-    public void test2(){
+    public int test2(int source, int target){
         countNode = 4;
         bandwidth = new int[countNode][countNode];
         flow = new int [countNode][countNode];
@@ -149,5 +173,66 @@ public class Graph {
         flow[0][2] = 8;
         flow[1][3] = 8;
         flow[2][1] = 8;
+
+        return findMaxFlow(source, target);
+    }
+
+    public int myVariantBandwidthZero(int source, int target){
+        countNode = 6;
+        bandwidth = new int[countNode][countNode];
+        flow = new int [countNode][countNode];
+        isSeen = new boolean[countNode];
+        bandwidth[0][1] = 10;
+        bandwidth[0][2] = 6;
+        bandwidth[0][5] = 7;
+
+        bandwidth[1][2] = 5;
+        bandwidth[1][3] = 7;
+
+        bandwidth[2][4] = 8;
+
+        bandwidth[3][1] = 7;
+        bandwidth[3][5] = 6;
+
+        bandwidth[4][2] = 8;
+        bandwidth[4][3] = 9;
+        bandwidth[4][5] = 9;
+
+        return findMaxFlow(source, target);
+    }
+
+    public int myVariantBandwidthNotZero(int source, int target){
+        countNode = 6;
+        bandwidth = new int[countNode][countNode];
+        flow = new int [countNode][countNode];
+        isSeen = new boolean[countNode];
+        bandwidth[0][1] = 8;
+        flow[0][1] = 2;
+        bandwidth[0][2] = 0;
+        flow[0][2] = 6;
+        bandwidth[0][5] = 7;
+        flow[0][5] = 0;
+
+        bandwidth[1][2] = 3;
+        flow[1][2] = 2;
+        bandwidth[1][3] = 7;
+        flow[1][3] = 0;
+
+        bandwidth[2][4] = 2;
+        flow[2][4] = 6;
+
+        bandwidth[3][1] = 7;
+        flow[3][1] = 0;
+        bandwidth[3][5] = 0;
+        flow[3][5] = 6;
+
+        bandwidth[4][2] = 8;
+        flow[4][2] = 0;
+        bandwidth[4][3] = 3;
+        flow[4][3] = 6;
+        bandwidth[4][5] = 7;
+        flow[4][5] = 2;
+
+        return findMaxFlow(source, target);
     }
 }
